20c5df15f3e22d2849574cb02975d23c3c01f584
Ilya Bogdanov <fumlead@gmail.com>
2017-05-14 18:40:11 +0300

    Bunch of refactoring

--- examples/drawing.rs
+++ examples/drawing.rs
@@ -5,9 +5,9 @@ use genial::draw;
 use genial::color;
 
 fn main() {
-    let mut image = Image::new(100, 100);
+    let mut image = Image::new(100, 100, genial::ColorFormat::RGB);
     draw::draw_line(&mut image, (0, 0), (100, 100), color::CYAN);
-    draw::draw_circle(&mut image, (50, 50), 30, color::LIME);
+    draw::draw_circle(&mut image, (50, 50), 30, color::YELLOW);
     draw::draw_filled_circle(&mut image, (50, 50), 20, color::MAROON);
     image.save_to_file("drawing.bmp");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,82 @@
 extern crate imagefmt;
-use imagefmt::{ColFmt, ColType};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct RGB(pub u8, pub u8, pub u8);
+pub struct RGB {
+    data: [u8; 3],
+}
+
+pub fn rgb(r: u8, g: u8, b: u8) -> RGB {
+    RGB { data: [r, g, b] }
+}
 
 pub mod color {
     use ::*;
-    pub const WHITE: RGB = RGB(255, 255, 255);
-    pub const BLACK: RGB = RGB(0, 0, 0);
-    pub const RED: RGB = RGB(255, 0, 0);
-    pub const LIME: RGB = RGB(0, 255, 0);
-    pub const BLUE: RGB = RGB(0, 0, 255);
-    pub const YELLOW: RGB = RGB(255, 255, 0);
-    pub const CYAN: RGB = RGB(0, 255, 255);
-    pub const MAGENTA: RGB = RGB(255, 0, 255);
-    pub const SILVER: RGB = RGB(192, 192, 192);
-    pub const GRAY: RGB = RGB(128, 128, 128);
-    pub const MAROON: RGB = RGB(218, 0, 0);
-    pub const OLIVE: RGB = RGB(128, 128, 0);
-    pub const GREEN: RGB = RGB(0, 128, 0);
-    pub const PURPLE: RGB = RGB(128, 0, 128);
-    pub const TEAL: RGB = RGB(0, 128, 128);
-    pub const NAVY: RGB = RGB(0, 0, 128);
+    pub const WHITE: RGB = RGB { data: [255, 255, 255] };
+    pub const BLACK: RGB = RGB { data: [0, 0, 0] };
+    pub const RED: RGB = RGB { data: [255, 0, 0] };
+    pub const LIME: RGB = RGB { data: [0, 255, 0] };
+    pub const BLUE: RGB = RGB { data: [0, 0, 255] };
+    pub const YELLOW: RGB = RGB { data: [255, 255, 0] };
+    pub const CYAN: RGB = RGB { data: [0, 255, 255] };
+    pub const MAGENTA: RGB = RGB { data: [255, 0, 255] };
+    pub const SILVER: RGB = RGB { data: [192, 192, 192] };
+    pub const GRAY: RGB = RGB { data: [128, 128, 128] };
+    pub const MAROON: RGB = RGB { data: [218, 0, 0] };
+    pub const OLIVE: RGB = RGB { data: [128, 128, 0] };
+    pub const GREEN: RGB = RGB { data: [0, 128, 0] };
+    pub const PURPLE: RGB = RGB { data: [128, 0, 128] };
+    pub const TEAL: RGB = RGB { data: [0, 128, 128] };
+    pub const NAVY: RGB = RGB { data: [0, 0, 128] };
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum ColorFormat {
+    Y,
+    YA,
+    AY,
+    RGB,
+    RGBA,
+    ARGB,
+    BGR,
+    BGRA,
+    ABGR,
+}
+
+impl ColorFormat {
+    fn channels(&self) -> usize {
+        match *self {
+            ColorFormat::Y => 1,
+            ColorFormat::YA | ColorFormat::AY => 2,
+            ColorFormat::RGB | ColorFormat::BGR => 3,
+            ColorFormat::ABGR | ColorFormat::ARGB | ColorFormat::BGRA | ColorFormat::RGBA => 4,
+        }
+    }
+}
+
+pub trait Pixel {
+    fn from_rgb(u8, u8, u8) -> Self;
+    fn color_fmt(&self) -> ColorFormat;
+    fn as_slice(&self) -> &[u8];
+}
+
+impl Pixel for RGB {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        rgb(r, g, b)
+    }
+    fn color_fmt(&self) -> ColorFormat {
+        ColorFormat::RGB
+    }
+    fn as_slice(&self) -> &[u8] {
+        &self.data
+    }
 }
 
 #[derive(Clone)]
 pub struct Image {
-    data: Vec<RGB>,
-    width: i32,
-    height: i32,
+    data: Vec<u8>,
+    width: usize,
+    height: usize,
+    format: ColorFormat,
 }
 
 fn swap<T: Clone>(i: &mut T, j: &mut T) {
@@ -38,74 +86,87 @@ fn swap<T: Clone>(i: &mut T, j: &mut T) {
 }
 
 impl Image {
-    pub fn new(width: i32, height: i32) -> Self {
-        let mut data = Vec::with_capacity(width as usize * height as usize);
-        for _ in 0..width * height {
-            data.push(RGB(0, 0, 0));
+    pub fn new(width: usize, height: usize, format: ColorFormat) -> Self {
+        let mut data = Vec::with_capacity(width as usize * height as usize *
+                                          format.channels() as usize);
+        for _ in 0..width * height * format.channels() {
+            data.push(0);
         }
-        debug_assert_eq!(data.len() as i32, width * height);
+        debug_assert_eq!(data.len(), width * height * format.channels());
         Image {
             data: data,
             width: width,
             height: height,
+            format: format,
         }
     }
 
     pub fn from_file(filename: &str) -> Option<Self> {
-        let image = imagefmt::read(filename, ColFmt::Auto);
+        let image = imagefmt::read(filename, imagefmt::ColFmt::Auto);
         match image {
             Ok(image) => {
                 let (width, height) = (image.w, image.h);
-                if image.fmt != ColFmt::RGB {
-                    return None;
-                }
-                let mut buf = Vec::with_capacity(width * height);
-                for c in image.buf.chunks(3) {
-                    let color = RGB(c[0], c[1], c[2]);
-                    buf.push(color);
-                }
+                let format = match image.fmt {
+                    imagefmt::ColFmt::ABGR => ColorFormat::ABGR,
+                    imagefmt::ColFmt::ARGB => ColorFormat::ARGB,
+                    imagefmt::ColFmt::AY => ColorFormat::AY,
+                    imagefmt::ColFmt::Y => ColorFormat::Y,
+                    imagefmt::ColFmt::YA => ColorFormat::YA,
+                    imagefmt::ColFmt::RGB => ColorFormat::RGB,
+                    imagefmt::ColFmt::RGBA => ColorFormat::RGBA,
+                    imagefmt::ColFmt::BGR => ColorFormat::BGR,
+                    imagefmt::ColFmt::BGRA => ColorFormat::BGRA,
+                    imagefmt::ColFmt::Auto => unimplemented!(),
+                };
                 Some(Image {
-                         width: width as i32,
-                         height: height as i32,
-                         data: buf,
+                         width: width,
+                         height: height,
+                         data: image.buf,
+                         format: format,
                      })
             }
             Err(_) => None,
         }
     }
 
-    pub fn dimensions(&self) -> (i32, i32) {
+    pub fn dimensions(&self) -> (usize, usize) {
         (self.width, self.height)
     }
 
-    pub fn set_pixel(&mut self, x: i32, y: i32, color: RGB) {
-        debug_assert!(x <= self.width);
-        debug_assert!(y <= self.height);
-        let index = (self.height - y - 1) * self.width + x;
-        self.data[index as usize] = color;
+    pub fn set_pixel<P: Pixel>(&mut self, x: i32, y: i32, color: P) {
+        debug_assert!(x <= self.width as i32);
+        debug_assert!(y <= self.height as i32);
+        let index = ((self.height as i32 - y - 1) * self.width as i32 + x) as usize *
+                    self.format.channels();
+        let channels = self.format.channels() as usize;
+        if color.color_fmt() == self.format {
+            for i in index..(index + channels) {
+                self.data[i] = color.as_slice()[i - index];
+            }
+        }
     }
 
-    pub fn get_pixel(&self, x: i32, y: i32) -> RGB {
-        debug_assert!(x <= self.width);
-        debug_assert!(y <= self.height);
-        let index = (self.height - y - 1) * self.width + x;
-        self.data[index as usize]
+    pub fn get_pixel<P: Pixel>(&self, x: i32, y: i32) -> P {
+        debug_assert!(x <= self.width as i32);
+        debug_assert!(y <= self.height as i32);
+        let index = ((self.height as i32 - y - 1) * self.width as i32 + x) as usize *
+                    self.format.channels() as usize;
+        match self.format {
+            ColorFormat::RGB => {
+                P::from_rgb(self.data[index], self.data[index + 1], self.data[index + 2])
+            }
+            _ => unimplemented!(),
+        }
     }
 
 
     pub fn save_to_file(&self, filename: &str) {
-        let mut buf = Vec::with_capacity(self.data.len() * 3);
-        for p in &self.data {
-            buf.push(p.0);
-            buf.push(p.1);
-            buf.push(p.2);
-        }
         imagefmt::write(filename,
                         self.width as usize,
                         self.height as usize,
-                        ColFmt::RGB,
-                        &buf,
-                        ColType::Color)
+                        imagefmt::ColFmt::RGB,
+                        &self.data,
+                        imagefmt::ColType::Color)
                 .unwrap();
     }
 }
@@ -114,11 +175,11 @@ pub mod ops {
     use ::*;
     pub fn flip_vertical(image: &Image) -> Image {
         let (width, height) = image.dimensions();
-        let mut result = Image::new(width, height);
+        let mut result = Image::new(width, height, image.format);
         for y in 0..height {
             for x in 0..width {
-                let p = image.get_pixel(x, y);
-                result.set_pixel(x, height - y - 1, p);
+                let p: RGB = image.get_pixel(x as i32, y as i32);
+                result.set_pixel(x as i32, (height - y) as i32 - 1, p);
             }
         }
         result
@@ -126,11 +187,11 @@ pub mod ops {
 
     pub fn flip_horizontal(image: &Image) -> Image {
         let (width, height) = image.dimensions();
-        let mut result = Image::new(width, height);
+        let mut result = Image::new(width, height, image.format);
         for y in 0..height {
             for x in 0..width {
-                let p = image.get_pixel(x, y);
-                result.set_pixel(width - x - 1, y, p);
+                let p: RGB = image.get_pixel(x as i32, y as i32);
+                result.set_pixel((width - x) as i32 - 1, y as i32, p);
             }
         }
         result
@@ -144,10 +205,10 @@ pub mod draw {
                      (mut x1, mut y1): (i32, i32),
                      color: RGB) {
         let dimensions = image.dimensions();
-        debug_assert!(x0 <= dimensions.0);
-        debug_assert!(x1 <= dimensions.0);
-        debug_assert!(y0 <= dimensions.1);
-        debug_assert!(y1 <= dimensions.1);
+        debug_assert!(x0 <= dimensions.0 as i32);
+        debug_assert!(x1 <= dimensions.0 as i32);
+        debug_assert!(y0 <= dimensions.1 as i32);
+        debug_assert!(y1 <= dimensions.1 as i32);
         let mut steep = false;
         if (x0 - x1).abs() < (y0 - y1).abs() {
             steep = true;
